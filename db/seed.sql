640f571ab0039ceb47eb136927cc82653503c85f
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-08-22 20:26:54 -0500

    added screenshot

--- db/seed.sql
+++ db/seed.sql
@@ -11,11 +11,11 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Web Dev', 85000, 1),
+('Full Stack Developer', 85000, 1),
 ('Accountant', 65000, 2),
 ('Lawyer', 105000, 3),
-('Manager', 65000, 4),
-('Human Resources', 55000, 5),
+('CEO', 150000, 4),
+('HR Coordinator', 55000, 5),
 ('Sales Rep', 40000, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
